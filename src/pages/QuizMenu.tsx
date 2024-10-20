fac968f21bbbec8573bb6e51d3e9ddf0e7f993b8
ssumanlife <ksm9805@naver.com>
2024-10-20 18:18:28 +0900

    feat/resetCorrect

--- src/pages/QuizMenu.tsx
+++ src/pages/QuizMenu.tsx
@@ -2,17 +2,24 @@ import { css } from "@emotion/react"
 import { data } from "@/data/mockData"
 import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom"
 import Button from "@/components/Button"
+import useNumOfCorrectStore from "@/stores/useNumOfCorrectStore"
+import { useEffect } from "react"
 
 const QuizMenu = () => {
   const { menu } = useParams()
   const location = useLocation()
   const navigate = useNavigate()
+  const resetCorrect = useNumOfCorrectStore((state) => state.resetCorrect)
+  const isQuizRoute = location.pathname.startsWith(`/${menu}/`)
+
+  useEffect(() => {
+    resetCorrect()
+  }, [isQuizRoute])
 
   const menuData = data[menu as keyof typeof data]
   if (!menuData) {
     return <p>데이터가 없습니다.</p>
   }
-  const isQuizRoute = location.pathname.startsWith(`/${menu}/`)
 
   return (
     <div css={quizMenuWrapper}>
