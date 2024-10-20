f0f5283f61f52c8ddd20a79330887e8cba94504e
ssumanlife <ksm9805@naver.com>
2024-10-20 16:12:51 +0900

    setting/ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
-export default {
+module.exports = {
   apps: [
     {
-      name: "fepick",
+      name: "app",
       script: "server/index.js",
       env: {
         NODE_ENV: "production",
-        VITE_MONGO_URL: process.env.VITE_MONGO_URL,
-        VITE_PORT: process.env.VITE_PORT,
+        PORT: process.env.VITE_PORT,
+        MONGO_URL: process.env.VITE_MONGO_URL,
       },
     },
   ],
